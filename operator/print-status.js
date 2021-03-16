276c1749fc6374081d902d02b7dd370bed7c74f6
Sebastián Claudio Nale <sebinale@gmail.com>
2021-03-16 17:33:06 -0300

    Update web3 and documentation in print-status.

--- operator/print-status.js
+++ operator/print-status.js
@@ -1,40 +1,54 @@
-var utils = require('../utils');
-var yargs = require('yargs');
+"use strict";
+
+const utils = require("../utils");
+const yargs = require("yargs");
 
 async function doIt() {
-  var argv = utils.completeYargs(yargs
-      .option('o', {
-        group: 'Data:',
-        alias: 'operatorPublicKeyHash',
-        describe: 'operator public key hash',
-        demandOption: true
+  const argv = utils.completeYargs(
+    yargs
+      .option("o", {
+        group: "Data:",
+        alias: "operatorPublicKeyHash",
+        describe: "operator public key hash",
+        demandOption: true,
       })
-      .usage('Prints operator status.\nUsage: node operator/print-status.js --ethnetwork <eth network> --operatorPublicKeyHash <operator public key hash>')
-      .example('node operator/print-status.js --ethnetwork rinkeby --operatorPublicKeyHash 0x03cd041b0139d3240607b9fd1b2d1b691e22b5d6')
-    ).argv;
+      .usage(
+        `Prints operator status.
+Usage: node operator/print-status.js --operatorPublicKeyHash <operator public key hash>`
+      )
+      .example(
+        "node operator/print-status.js --operatorPublicKeyHash 0x03cd041b0139d3240607b9fd1b2d1b691e22b5d6",
+        "Show the status of operator 0x03cd041b0139d3240607b9fd1b2d1b691e22b5d6"
+      )
+  ).argv;
 
-  var initObjects = utils.init(argv);
-  var web3 = initObjects.web3;
-  var DogeToken = initObjects.DogeToken;
+  const { web3, dogeToken } = utils.init(argv);
 
-  var operatorPublicKeyHash = argv.operatorPublicKeyHash;
+  const operatorPublicKeyHash = argv.operatorPublicKeyHash;
 
-  console.log("Print operator status for operator with public key hash " + operatorPublicKeyHash);
+  console.log(
+    `Print operator status for operator with public key hash ${operatorPublicKeyHash}`
+  );
 
   // Do some checks
-  if (!await utils.doSomeChecks(web3)) {
-    return;
-  }
+  await utils.doSomeChecks(web3);
 
-  var dt = await DogeToken.deployed();
-  var operator = await dt.operators.call(operatorPublicKeyHash);     
-  console.log("Eth Address : " + operator[0].toString(16));       
-  console.log("Doge available balance : " + utils.satoshiToDoge(operator[1].toNumber()) + " doges." );
-  console.log("Doge pending balance : " + utils.satoshiToDoge(operator[2].toNumber())  + " doges." );
-  console.log("Deposit : " + web3.fromWei(operator[4].toNumber())  + " eth.");       
-  var operatorKeyIndex = operator[5];
-  let operatorKey = await dt.operatorKeys(operatorKeyIndex);
-  console.log("Active : " + (operatorKey[1] == false));
+  const operator = await dogeToken.operators.call(operatorPublicKeyHash);
+  console.log(`Eth Address: ${operator[0].toString(16)}`);
+  console.log(
+    `Doge available balance: ${utils.satoshiToDoge(
+      operator[1].toNumber()
+    )} doges.`
+  );
+  console.log(
+    `Doge pending balance: ${utils.satoshiToDoge(
+      operator[2].toNumber()
+    )} doges.`
+  );
+  console.log(`Deposit: ${web3.utils.fromWei(operator[4])} eth.`);
+  const operatorKeyIndex = operator[5];
+  const operatorKey = await dogeToken.operatorKeys(operatorKeyIndex);
+  console.log(`Active: ${operatorKey[1] == false}`);
 }
 
 doIt();
