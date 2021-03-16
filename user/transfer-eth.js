535ebc9689ebf806ff54740bcdb6c538a0086d98
Sebastián Claudio Nale <sebinale@gmail.com>
2021-03-16 14:15:37 -0300

    Cleanup usage documentation and update web3 in transfer eth operation.

--- user/transfer-eth.js
+++ user/transfer-eth.js
@@ -6,10 +6,11 @@ const yargs = require("yargs");
 async function doIt() {
   const argv = utils.completeYargs(
     yargs
-      .option("s", {
+      .option("pk", {
         group: "Data:",
-        alias: "sender",
-        describe: "from eth address",
+        alias: "privateKey",
+        type: "string",
+        describe: "private key used to sign the transaction",
         demandOption: true,
       })
       .option("r", {
@@ -27,21 +28,26 @@ async function doIt() {
       })
       .usage(
         `Transfers eth from one eth address to another.
-Usage: node user/transfer-eth.js --ethnetwork <eth network> --sender <from eth address> --receiver <to eth address> --value <number of weis>`
+Usage: node user/transfer-eth.js --privateKey <sender eth private key> --receiver <to eth address> --value <number of weis>`
       )
       .example(
-        "node user/transfer-eth.js --ethnetwork rinkeby --sender 0xd2394f3fad76167e7583a876c292c86ed10305da --receiver 0xd2394f3fad76167e7583a876c292c86ed1ffffff --value 10000000000000000"
+        "node user/transfer-eth.js --privateKey 0xf968fec769bdd389e33755d6b8a704c04e3ab958f99cc6a8b2bcf467807f9634 --receiver 0xd2394f3fad76167e7583a876c292c86ed1ffffff --value 10000000000000000",
+        "Send 0.01 ETH to 0xd2394f3fad76167e7583a876c292c86ed1ffffff"
       )
   ).argv;
 
   const { web3 } = utils.init(argv);
 
-  const sender = argv.sender;
+  const privateKey = argv.privateKey;
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  web3.eth.accounts.wallet.add(account);
+  const sender = account.address;
+
   const receiver = argv.receiver;
   const value = argv.value;
 
   console.log(
-    `Transfer ${web3.fromWei(value)} eth from ${sender} to ${receiver}`
+    `Transfer ${web3.utils.fromWei(value)} eth from ${sender} to ${receiver}`
   );
 
   // Do some checks
@@ -52,10 +58,10 @@ Usage: node user/transfer-eth.js --ethnetwork <eth network> --sender <from eth a
 
   // Do transfer
   let senderEthBalance = await web3.eth.getBalance(sender);
-  // console.log(`Sender eth balance: ${web3.fromWei(senderEthBalance.toNumber())}`);
+  // console.log(`Sender eth balance: ${web3.utils.fromWei(senderEthBalance)}`);
   let receiverEthBalance = await web3.eth.getBalance(receiver);
   console.log(
-    `Receiver balance: ${web3.fromWei(receiverEthBalance.toNumber())} eth.`
+    `Receiver balance: ${web3.utils.fromWei(receiverEthBalance)} eth.`
   );
   console.log("Sending transaction...");
   await web3.eth.sendTransaction({
@@ -66,11 +72,11 @@ Usage: node user/transfer-eth.js --ethnetwork <eth network> --sender <from eth a
   console.log("Transaction sent.");
   senderEthBalance = await web3.eth.getBalance(sender);
   console.log(
-    `Sender balance: ${web3.fromWei(senderEthBalance.toNumber())} eth.`
+    `Sender balance: ${web3.utils.fromWei(senderEthBalance)} eth.`
   );
   receiverEthBalance = await web3.eth.getBalance(receiver);
   console.log(
-    `Receiver balance: ${web3.fromWei(receiverEthBalance.toNumber())} eth.`
+    `Receiver balance: ${web3.utils.fromWei(receiverEthBalance)} eth.`
   );
   console.log("Transfer eth completed.");
 }
