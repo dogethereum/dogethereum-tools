b1a80e4d86f02e4d35e2e4fd766d1b7f13de28a5
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2021-03-16 16:36:31 -0300

    Remove bitcore-message dependency

--- operator/addoperator.js
+++ operator/addoperator.js
@@ -2,7 +2,6 @@ var utils = require('../utils');
 var yargs = require('yargs');
 var bitcoreLib = require('bitcore-lib');
 var ECDSA = bitcoreLib.crypto.ECDSA;
-var bitcoreMessage = require('bitcore-message');
 
 
 
