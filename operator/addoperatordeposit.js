ee0842b18d3ec542cce030007a9a7e7e3f1e0064
Sebastián Claudio Nale <sebinale@gmail.com>
2021-03-16 17:34:43 -0300

    Update web3 and usage documentation in add operator deposit.

--- operator/addoperatordeposit.js
+++ operator/addoperatordeposit.js
@@ -1,68 +1,100 @@
-var utils = require('../utils');
-var yargs = require('yargs');
+"use strict";
+
+const utils = require("../utils");
+const yargs = require("yargs");
 
 async function doIt() {
-  var argv = utils.completeYargs(yargs
-      .option('o', {
-        group: 'Data:',
-        alias: 'operatorPublicKeyHash',
-        describe: 'operator public key hash',
-        demandOption: true
+  const argv = utils.completeYargs(
+    yargs
+      .option("o", {
+        group: "Data:",
+        alias: "operatorPublicKeyHash",
+        describe: "operator public key hash",
+        demandOption: true,
       })
-      .option('v', {
-        group: 'Data:',
-        alias: 'value',
-        describe: 'number of weis to deposit',
-        type: 'number',
-        demandOption: true
+      .option("v", {
+        group: "Data:",
+        alias: "value",
+        describe: "number of weis to deposit",
+        type: "number",
+        demandOption: true,
       })
-      .option('a', {
-        group: 'Data:',
-        alias: 'ethAddress',
-        describe: 'operator eth address',
-        demandOption: true
+      .option("pk", {
+        group: "Data:",
+        alias: "privateKey",
+        describe: "private key used to sign the transaction",
+        demandOption: true,
       })
-      .usage('Transfers eth from the operator account to the DogeToken contract.\nUsage: node operator/addoperatordeposit.js --ethnetwork <eth network> --operatorPublicKeyHash <operator public key hash> --value <number of weis to deposit> --ethAddress <operator eth address>')
-      .example('node operator/addoperatordeposit.js --ethnetwork rinkeby --operatorPublicKeyHash 0x03cd041b0139d3240607b9fd1b2d1b691e22b5d6 --value 1000000000000000000 --ethAddress 0xd2394f3fad76167e7583a876c292c86ed1ffffff')
-    ).argv;
+      .usage(
+        `Transfers eth from the operator account to the DogeToken contract.
+Usage: node operator/addoperatordeposit.js --operatorPublicKeyHash <operator public key hash> --value <number of weis to deposit> --privateKey <operator eth private key>`
+      )
+      .example(
+        "node operator/addoperatordeposit.js --operatorPublicKeyHash 0x03cd041b0139d3240607b9fd1b2d1b691e22b5d6 --value 1000000000000000000 --privateKey 0xf968fec769bdd389e33755d6b8a704c04e3ab958f99cc6a8b2bcf467807f9634",
+        "Add 1 ETH to the deposit held by the DogeToken contract for the operator of private key 0xf968fec769bdd389e33755d6b8a704c04e3ab958f99cc6a8b2bcf467807f9634"
+      )
+  ).argv;
 
-  var initObjects = utils.init(argv);
-  var web3 = initObjects.web3;
-  var DogeToken = initObjects.DogeToken;
+  const { web3, dogeToken } = utils.init(argv);
 
-  var operatorPublicKeyHash = argv.operatorPublicKeyHash;
-  var value = argv.value;
-  var operatorEthAddress = argv.ethAddress;
+  const operatorPublicKeyHash = argv.operatorPublicKeyHash;
+  const value = argv.value;
+  const privateKey = argv.privateKey;
 
-  console.log("Add operator deposit with public key hash " + operatorPublicKeyHash + ", value " + web3.fromWei(value) + " eth and eth address " + operatorEthAddress);
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  web3.eth.accounts.wallet.add(account);
+  const operatorEthAddress = account.address;
+
+  console.log(
+    `Add operator deposit with public key hash ${operatorPublicKeyHash}, value ${web3.utils.fromWei(
+      value
+    )} eth and eth address ${operatorEthAddress}`
+  );
 
   // Do some checks
-  if (!await utils.doSomeChecks(web3, operatorEthAddress)) {
-    return;
-  }
-  if(!(value > 0)) {
+  await utils.doSomeChecks(web3, operatorEthAddress);
+  if (!(value > 0)) {
     console.log("Value should be greater than 0");
     return;
   }
 
-  var dt = await DogeToken.deployed();
-
-  await printOperatorDeposit(web3, dt, operatorPublicKeyHash, "Current operator deposit : ");
+  await printOperatorDeposit(
+    web3,
+    dogeToken,
+    operatorPublicKeyHash,
+    "Current operator deposit"
+  );
 
   // Add operator deposit
-  console.log("Adding operator deposit... ");
-  var dt = await DogeToken.deployed();
-  const addOperatorDepositTxReceipt = await dt.addOperatorDeposit(operatorPublicKeyHash, {value: value, from : operatorEthAddress, gas: 50000, gasPrice: argv.gasPrice});
+  console.log("Adding operator deposit...");
+  // TODO: check gas costs
+  const addOperatorDepositTxReceipt = await dogeToken.addOperatorDeposit(
+    operatorPublicKeyHash,
+    {
+      value: value,
+      from: operatorEthAddress,
+      gas: 50000,
+      gasPrice: argv.gasPrice,
+    }
+  );
   utils.printTxResult(addOperatorDepositTxReceipt, "Add operator deposit");
 
-  await printOperatorDeposit(web3, dt, operatorPublicKeyHash, "Updated operator deposit : ");
+  await printOperatorDeposit(
+    web3,
+    dogeToken,
+    operatorPublicKeyHash,
+    "Updated operator deposit"
+  );
 }
 
-async function printOperatorDeposit(web3, dt, operatorPublicKeyHash, label) {
-  var operator = await dt.operators.call(operatorPublicKeyHash);     
-  console.log(label + web3.fromWei(operator[4].toNumber())  + " eth.");       
+async function printOperatorDeposit(
+  web3,
+  dogeToken,
+  operatorPublicKeyHash,
+  label
+) {
+  const operator = await dogeToken.operators.call(operatorPublicKeyHash);
+  console.log(`${label}: ${web3.utils.fromWei(operator[4])} eth.`);
 }
-
-
 
 doIt();
