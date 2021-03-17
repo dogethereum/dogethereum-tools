c8ba9424e7cb9903b15847e431685aea6a2d08fe
Sebastián Claudio Nale <sebinale@gmail.com>
2021-03-17 14:56:46 -0300

    Add a dogecoin key conversion script.

---
+++ user/convert-doge-key-to-eth.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const utils = require("../utils");
+const yargs = require("yargs");
+
+const { keyDogeToEthInBytes } = require("./formatconverter");
+
+async function doIt() {
+  const argv = utils.completeYargs(
+    yargs
+      .option("dk", {
+        group: "Data:",
+        alias: "dogePrivateKey",
+        describe: "private key in dogecoin format",
+        demandOption: true,
+        type: "string",
+      })
+      .usage(
+        `Converts a private key in dogecoin format to the standard ethereum format.
+Usage: node user/convert-doge-key-to-eth.js --privateKey <private key in eth format>`
+      )
+      .example(
+        "node user/convert-doge-key-to-eth.js --dogePrivateKey Kdef456def456def456def456def456def456def456def456def",
+        "Converts the Kdef456def456def456def456def456def456def456def456def dogecoin key to an ethereum key."
+      )
+  ).argv;
+
+  const { web3 } = utils.init(argv);
+
+  const dogePrivateKey = argv.dogePrivateKey;
+  const privateKey = `0x${keyDogeToEthInBytes(dogePrivateKey).toString("hex")}`;
+
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+
+  console.log(`Got key for Ethereum address ${dogeTokenHolderAddress}.
+Ethereum private key: ${privateKey}`);
+}
+
+doIt();
